231dc1b59a31379b0062b935ca590fbba002ded8
Srijan Roy <srijanroy@Srijans-MacBook-Air.local>
2019-04-07 19:48:03 +0530

    Second Commit

--- FetchBook-API/UserAccessAPI/routes/app.js
+++
@@ -1,6 +0,0 @@
-module.exports=function(app){
-    var userCon=require('../Controller/UserController')
-    console.log('entered app')
-    app.route('/lost').post(userCon.signUp)
-    app.route('/book').post(userCon.fetchBook)
-}
